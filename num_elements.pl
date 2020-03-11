3d7862277bd0f6c9d27faaecc981b5bbd6243873
agnespie <61785669+agnespie@users.noreply.github.com>
2020-03-11 14:31:25 +0100

    Add files via upload

---
+++ num_elements.pl
@@ -0,0 +1,17 @@
+/* 
+num_elements(+List, -Number)
+it is true if Number unify with the Number of elements of List
+
+?- mum_elements([1,2,3], N).
+N = 3
+
+?- num_elements([1,2,3], 3).
+yes
+
+?- num_elements(List, 3).
+
+*/
+
+num_elements([], 0).
+
+num_elements([Head|Tail],   R):- num_elements(Tail, N) R is N+1
